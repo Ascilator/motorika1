702a1f08b6208c3e8becefc6ac268f4c94fad0da
Ascialtor <uladzislau.yarmashuk@innowise-group.com>
2023-11-29 18:51:28 +0300

    adsf

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,8 @@ $(function () {
     $('body').toggleClass('_lock');
     $('.burger').toggleClass('_active');
   });
+
+  setInterval(() => {
+    $('._app').toggleClass('_active');
+  }, 4000);
 });
